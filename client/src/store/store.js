c6c2427267c3b9d5bb5596b03eb06dc54e34eae8
Jorge Guazzini <jorge.guazzini.y@gmail.com>
2022-07-17 02:53:34 -0400

    actions done

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,10 @@
 import { applyMiddleware, compose} from 'redux';
 import { createStore } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from 'redux-thunk';
+
 
 const store = createStore(
-    rootReducer,
+    
     compose(
         applyMiddleware(thunk)
     )
